56be48dabb25b2a27e4f49f45aa8a69695dceacf
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-06-10 19:04:14 +0700

    upload pertama

---
+++ contoh/contoh-gabungan-style.js
@@ -0,0 +1,10 @@
+var blackBox = document.getElementById('black-box');
+var jumpBox = document.getElementById('box-jump');
+jumpBox.addEventListener('click', function() {
+  blackBox.style.background = 'green';
+  blackBox.style.width = '400px';
+  setTimeout(function() {
+    blackBox.style.background = 'black';
+    blackBox.style.width = '100px';
+  }, 500);
+});
